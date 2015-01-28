aba7ea5965be57acd1ee7503250fdc4d723d39f5
dodolong <mctt90@gmail.com>
2015-01-28 11:48:51 +0800

    update

---
+++ src/mrml/mappers_and_reducers.h
@@ -0,0 +1,9 @@
+// Copyright 2015 PKU-Cloud.
+// Author: Chao Ma (mctt90@gmail.com)
+//
+// Pre-defined mappers and reducers
+//
+#ifndef MRML_MAPPERS_AND_REDUCERS_H_
+#define MRML_MAPPERS_AND_REDUCERS_H_
+
+#endif // MRML_MAPPERS_AND_REDUCERS_H_
