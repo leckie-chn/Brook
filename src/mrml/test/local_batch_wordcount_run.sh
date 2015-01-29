34e4a2c27ea70d48cd535f83d17d8e209c4e2451
dodolong <mctt90@gmail.com>
2015-01-29 19:44:17 +0800

    update

--- src/mrml/test/local_batch_wordcount_run.sh
+++ src/mrml/test/local_batch_wordcount_run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Clean the tmp files such as output file and log files which last execution generated.
+# Clean the tmp files such as output files and log files which last execution generated.
 rm -rf /tmp/*
 
 # Run local wordcount.
 mpiexec -n 3 ./mrml_main                               \
     --num_agent_workers=2                              \
-    --num_server_workers=1                             \
+    --num_server_workers=1                             \ 
     --agent_workers="1,2"                              \
     --server_workers="0"                               \
     --mapper_class="WordCountMapper"                   \
-    --reducer_class="SumIntegerReducer"                \
+    --reducer_class="BatchIntegerSumReducer"           \
     --cache_file_value="/Users/alex/wordcount/text-*"  \
-    --cache_file_model="/tmp/model"                    \
+    --cache_file_model="/tmp/output"                   \
     --output_files="/tmp/output"                       \
     --input_format="text"                              \
     --output_format="text"                             \
-    --log_filebase="/tmp/log"
+    --log_filebase="/tmp/log"                          \
+    --batch_reduction=true
