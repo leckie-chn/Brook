b3b4ac44f70d16e685b6fd7cdaae5dc7c705af4b
dodolong <mctt90@gmail.com>
2015-01-30 22:30:09 +0800

    update

--- src/in_memory_store/dense_vector_test.cc
+++ src/in_memory_store/dense_vector_test.cc
@@ -47,3 +47,33 @@ TEST(DenseVector, AddScaled) {
     EXPECT_EQ(u[1], 1);
     EXPECT_EQ(u[2], 2);
 }
+
+TEST(DenseVector, AddScaledInto) {
+    RealVector w, u, v;
+    w.resize(3, 1);
+    u.push_back(2);
+    u.push_back(4);
+    u.push_back(6);
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(0);
+    AddScaledInto(&w, u, v, 0.5);
+    EXPECT_EQ(w.size(), 3);
+    EXPECT_EQ(w[0], 3);
+    EXPECT_EQ(w[1], 6);
+    EXPECT_EQ(w[2], 6);
+}
+
+TEST(DenseVector, DotProduct) {
+    RealVector v, u, w;
+    v.push_back(1);
+    v.push_back(0);
+    u.push_back(0);
+    u.push_back(1);
+    w.push_back(1);
+    w.push_back(1);
+    EXPECT_EQ(DotProduct(v, u), 0);
+    EXPECT_EQ(DotProduct(u, v), 0);
+    EXPECT_EQ(DotProduct(v, w), 1);
+    EXPECT_EQ(DotProduct(u, w), 1);
+}
