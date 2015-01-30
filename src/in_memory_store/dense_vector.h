e5e6c3449b625ee4cdee0f65432b5ef45ea0b96d
dodolong <mctt90@gmail.com>
2015-01-30 20:14:14 +0800

    update

--- src/in_memory_store/dense_vector.h
+++ src/in_memory_store/dense_vector.h
@@ -1,3 +1,23 @@
-#ifndef _DENSE_VECTOR_H
-#define _DENSE_VECTOR_H
-#endif
+// Copyright 2015 PKU-Cloud.
+// Author : Chao Ma (mctt90@gmail.com)
+//
+#ifndef IN_MEMORY_STORE_DENSE_VECTOR_H_
+#define IN_MEMORY_STORE_DENSE_VECTOR_H_
+
+#include <vector>
+
+namespace brook {
+
+using std::ostream;
+using std::vector;
+
+template <class ValueType>
+class DenseVector : public vector<ValueType> {
+public:
+
+};
+
+
+} // namespace brook
+
+#endif // IN_MEMORY_STORE_DENSE_VECTOR_H_
