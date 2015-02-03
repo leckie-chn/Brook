acb6300fa850df8c043f20676fe4dacd5cb9d086
dodolong <mctt90@gmail.com>
2015-02-03 21:13:33 +0800

    update

--- src/pre_defined/message_define.h
+++ src/pre_defined/message_define.h
@@ -15,7 +15,7 @@ namespace brook {
 class String_Int_Message : public Message<std::string, uint32> {}
 class Int_Double_Message : public Message<uint32, double> {}
 class Int_Float_Message : public Message<uint32, float> {}
-class Int_String_Message : public Message<uint32, std::string> {} 
+class Int_String_Message : public Message<uint32, std::string> {}
 class String_String_Message : public Message<std::string, std::string> {}
 
 } // namespace brook
