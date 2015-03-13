eba1f0fe13623a564cd01673c7710f6edfa1ef80
dodolong <mctt90@gmail.com>
2015-03-13 17:03:39 +0800

    update

--- src/message/message_test.cc
+++ src/message/message_test.cc
@@ -3,6 +3,33 @@
 //
 #include "src/message/message.pb.h"
 
+#include <vector>
+#include <string>
+
+#include "src/base/common.h"
 #include "gtest/gtest.h"
 
-TEST(Message, )
+using namespace std;
+
+using brook::ValueList;
+using brook::SendMessage;
+
+TEST(SendMessage, basic) {
+    vector<double> vec_list;
+    vec_list.push_back(1.1);
+    vec_list.push_back(0.5);
+    SendMessage sm;
+    sm.mutable_list()->Clear();
+    uint64 index = 5;
+    sm.set_index(index);
+    for (int i = 0 ; i < vec_list.size() ; i++) {
+        sm.mutable_list()->add_value(vec_list[i]);
+    }
+    string output_buffer;
+    sm.SerializeToString(&output_buffer);
+    SendMessage recv_message;
+    recv_message.ParseFromString(output_buffer);
+    EXPECT_EQ(recv_message.index(), 5);
+    
+}
+
