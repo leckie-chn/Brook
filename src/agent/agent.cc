eab5c70ea1493777fb71cb05d6e0f2c24e6e2df2
dodolong <mctt90@gmail.com>
2015-03-17 19:00:24 +0800

    update

--- src/agent/agent.cc
+++ src/agent/agent.cc
@@ -24,68 +24,6 @@ using std::map;
 using std::string;
 using std::vector;
 
-//-------------------------------------------------------
-// Constants used by agent:
-//-------------------------------------------------------
-
-const int kAgentSendTag = 1;
-
-//-------------------------------------------------------
-// Initialization and Finalization of agent
-//-------------------------------------------------------
-
-bool AgentInitialize() {
-    LOG(INFO) << "Agent Initialize.";
-
-    return true;
-}
-
-int Shard(uint64 index) {
-    return (index / ChunkSize()) + NumAgentWorkers();
-}
-
-void AgentWorkerNotifyFinished() {
-    SendMessage sm;
-    sm.set_agent_worker(WorkerId());
-    string ssm;
-    sm.SerializeToString(&ssm);
-    // Send final signal to all server workers.
-    for (int server_id = NumAgentWorkers() ; server_id < NumWorkers() ; server_id++) {
-        MPI_Send(const_cast<char*>(ssm.data()), ssm.size(), MPI_CHAR,
-                 server_id, kAgentSendTag, MPI_COMM_WORLD);
-    }
-}
-
-//-------------------------------------------------------
-// Implementation of agent send work.
-//-------------------------------------------------------
-void SendWork() {
-    // By default, we create a TextReader.
-    // Maybe we will implemente the RecordReader in the future.
-    scoped_ptr<Reader> reader(new TextReader());
-    if (reader.get() == NULL) {
-        LOG(FATAL) << "Creating reader error.";
-    }
-    reader->Open(SubDataCacheFile().c_str());
-
-    SendMessage sm;
-    while (true) {
-        if (!reader->Read(&sm)) {
-            break;
-        }
-        string ssm;
-        sm.SerializeToString(&ssm);
-        // Send message to server
-        MPI_Send(const_cast<char*>(ssm.data()), ssm.size(), MPI_CHAR,
-                 Shard(sm.index()), kAgentSendTag, MPI_COMM_WORLD);
-    }
-    // Important to tell server worekrs to terminate.
-    AgentWorkerNotifyFinished();
-
-    LOG(INFO) << "agent send work succssed.";
-}
-
-
 void AgentService() {
 
     if (!AgentInitialize()) {
@@ -95,7 +33,7 @@ void AgentService() {
     LOG(INFO) << "Agent service started!";
 
     // TEST
-    SendWork();
+    
     // TEST
 
     AgentFinalize();
--- src/server/server.cc
+++ src/server/server.cc
@@ -19,6 +19,7 @@
 #include "src/strutil/join_strings.h"
 #include "src/sorted_buffer/sorted_buffer.h"
 #include "src/sorted_buffer/sorted_buffer_iterator.h"
+#include "src/in_memory_store/dense_table.h"
 
 namespace brook {
 
@@ -28,19 +29,6 @@ using std::vector;
 using sorted_buffer::SortedBuffer;
 using::sorted_buffer::SortedBufferIteratorImpl;
 
-//-------------------------------------------------------------------------------------
-// Constants used by server:
-//-------------------------------------------------------------------------------------
-
-const int kServerRecvTag = 1;
-
-
-//-------------------------------------------------------------------------------------
-// The buffer used by a server worker process to recieve a agent output message.
-//-------------------------------------------------------------------------------------
-char* output_recieve_buffer = NULL;
-
-
 bool ServerInitialize() {
     
     LOG(INFO) << "Server initialize.";
@@ -49,107 +37,9 @@ bool ServerInitialize() {
 }
 
 void ServerFinalize() {
-
     LOG(INFO) << "Server finalize.";
-
 }
 
-void ReceiveWork() {
-    LOG(INFO) << "Receive in " 
-              << (SortBasedShuffle() ? "sorted_shuffle" : "hash_shuffle")
-              << " model.";
-    LOG(INFO) << "Output to " << JoinStrings(OutputFiles(), ",");
-
-    // Once a agent worker finished processing its its shard, it will
-    // send a 'finished' message to all server workers. A server worker
-    // collects sucj messages into finished_agent_worker. After 'finished'
-    // message from all agent workers are collectedm the receive work quits.
-    std::set<int> finished_agent_workers;
-
-    // Used for Sorted_based shuffle
-    SortedBuffer* server_input_buffer = NULL;
-
-    // Used for Hash_based shuffle
-    typedef map<uint64, double> AggregationTable;
-    AggregationTable* aggregation_table = NULL;
-
-    // Initialize aggregation_table or sorted_buffer.
-    if (!SortBasedShuffle()) {
-        aggregation_table = new AggregationTable;
-    } else {
-        try {
-            LOG(INFO) << "Creating server input buffer ... filebase = "
-                      << ServerInputBufferFilebase()
-                      << ", buffer size cap = "
-                      << ServerInputBufferSize();
-            server_input_buffer = new SortedBuffer(
-                ServerInputBufferFilebase(),
-                ServerInputBufferSize());
-        } catch (const std::bad_alloc&) {
-            LOG(FATAL) << "Insufficient memory for creating server input buffer.";
-        }
-        LOG(INFO) << "Succeeded creating server input buffer.";
-    }
-
-    // Allocate agent outputs recieving buffer.
-    LOG(INFO) << "Creating agent output recieving buffer ...";
-    CHECK_LT(0, MaxAgentOutputBufferSize());
-    try {
-        output_recieve_buffer = new char[MaxAgentOutputBufferSize()];
-    } catch (const std::bad_alloc&) {
-        LOG(FATAL) << "Insufficient memory for agent output recieving buffer.";
-    }
-    LOG(INFO) << "Succeeded creating agent output recieving buffer.";
-
-    // Loop over agent outputs arrived in this server worker.
-    LOG(INFO) << "Start recieving and processing arriving agent outputs ...";
-    MPI_Status status;
-
-    int32 recieved_bytes = 0;
-
-    while (true) {
-        MPI_Recv(output_recieve_buffer, 
-                 MaxAgentOutputBufferSize(),
-                 MPI_CHAR,
-                 MPI_ANY_SOURCE,
-                 kServerRecvTag, MPI_COMM_WORLD, &status);
-    
-        MPI_Get_count(&status, MPI_CHAR, &recieved_bytes);
-
-        if (recieved_bytes >= MaxAgentOutputBufferSize()) {
-            LOG(FATAL) << "MPI_Recieving a proto message with size (at least) "
-                       << recieved_bytes
-                       << ", which >= FLAGS_max_output_size ("
-                       << MaxAgentOutputBufferSize() << ")."
-                       << "You can modify FLAGS_max_output_size defined in "
-                       << __FILE__;
-        }
-
-        SendMessage sm;
-        CHECK(sm.ParseFromArray(output_recieve_buffer,
-                            recieved_bytes));
-
-        // Final signal
-        if (sm.has_agent_worker()) {
-            finished_agent_workers.insert(sm.agent_worker());
-            if (finished_agent_workers.size() >= NumAgentWorkers()) {
-                LOG(INFO) << "Finished recieving agent agent outputs.";
-                break; // Break the while (true) loop.
-            }
-        } else {
-            CHECK(sm.has_index());
-            CHECK(sm.has_list());
-
-            // Hash_based shuffle
-            if (!SortBasedShuffle()) {
-                
-            } else { // Sorted_based shuffle
-
-            }
-        }
-    }
-    
-}
 
 void ServerService() {
     
