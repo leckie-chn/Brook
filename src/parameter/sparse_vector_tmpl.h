28d26a70a095b768d71dbe26f1bee48377860dc3
dodolong <mctt90@gmail.com>
2015-03-20 23:54:10 +0800

    update

--- src/parameter/sparse_vector_tmpl.h
+++ src/parameter/sparse_vector_tmpl.h
@@ -21,6 +21,45 @@ using std::ostream;
 // The ValueType must be a numberical type supporting const 0.
 template<class KeyType, class ValueType>
 class SparseVectorImpl : public map<KeyType, ValueType> {
+public:
+    typedef typename map<KeyType, ValueType>::const_iterator const_iterator;
+    typedef typename map<KeyType, ValueType>::iterator iterator;
+
+    // We constrain operator[] a read-only operation to prevent
+    // accidential insert of elements.
+    const ValueType& operator[](const KeyType& key) const {
+        const_iterator iter = this->find(key);
+        if (iter == this->end()) {
+            return zero_;
+        }
+        return iter->second;
+    }
+
+    // Set a value at given key. If value == 0, an existing key-value
+    // pair is removed. If value != 0, the value is set or inserted.
+    // This function alse serves as a convenient from of insert(),
+    // no need to use std::pair;
+    void set(const KeyType& key, const ValueType& value) {
+        iterator iter = this->find(key);
+        if (iter != this->end()) {
+            if (IsZero(value)) {
+                this->erase(iter);
+            } else {
+                iter->second = value;
+            }
+        } else {
+            if (!IsZero()) {
+                this->insert(pair<KeyType, ValueType>(key, value));
+            }
+        }
+    }
+
+    bool has(const KeyType& key) const {
+        return this->find(key) != this->end();
+    }
+
+protected:
+
 
 };
 
