f8a475c4f65954778165ae39dc37d189a3ad9898
dodolong <mctt90@gmail.com>
2015-03-21 21:20:58 +0800

    update

--- src/parameter/dense_matrix_tmpl.h
+++ src/parameter/dense_matrix_tmpl.h
@@ -18,13 +18,17 @@ using std::vector;
 template<class ValueType>
 class DenseMatrixImpl {
 public:
-    typedef typename DenseVectorImpl<ValueType> DV;
+    typedef DenseVectorImpl<ValueType> DV;
 
     DenseMatrixImpl(size_t row_size, size_t col_size, const ValueType& init)
     : vector<DV>(row_size, DV(col_size, init)) {}
 
     DenseMatrixImpl()
     : vector<DV>() {}
+
+    void Add(const DenseMatrixImpl<ValueType>&);
+
+    void Minus(const DenseMatrixImpl<ValueType>&);
 
     void Scale(const ValueType&);
 
@@ -38,9 +42,78 @@ public:
                        const DenseMatrixImpl<ValueType>&,
                        const ValueType&);
 
+    size_t RowSize() { return matrix_.size(); }
+   
+    const DV& operator[](size_t i) const {
+        CHECK_LE(0, i);
+        CHECK_LT(i, matrix_.size());
+        return matrix_[i];
+    }
+
 protected:
     vector<DV> matrix_;
 };
+
+// Add(v) : this <- this + v
+template<class ValueType>
+void DenseMatrixImpl<ValueType>::Add(const DenseMatrixImpl<ValueType>& v) {
+    size_t row_size = this->RowSize();
+    CHECK_EQ(row_size, v.RowSize());
+    for (size_t row = 0 ; row < row_size ; ++row) {
+        (*this)[row].Add(v[row]);
+    }
+}
+
+// Minus(v) : this <- this - v
+template<class ValueType>
+void DenseMatrixImpl<ValueType>::Minus(const DenseMatrixImpl<ValueType>& v) {
+    size_t row_size = this->RowSize();
+    CHECK_EQ(row_size, v.size());
+    for (size_t row = 0 ; row < row_size ; ++row) {
+        (*this)[row].Minus(v[row]);
+    }
+}
+
+// Scale(c) : this <- this * c
+template<class ValueType>
+void DenseMatrixImpl<ValueType>::Scale(const ValueType& c) {
+    size_t row_size = this->RowSize();
+    for (size_t row = 0 ; row < row_size ; ++row) {
+        (*this)[row].Scale(c);
+    }
+}
+
+// ScaleInto(v, c) : this <- v * c
+template<class ValueType>
+void DenseMatrixImpl<ValueType>::ScaleInto(const DenseMatrixImpl<ValueType>& v,
+                                           const ValueType& c) {
+    size_t row_size = this->RowSize();
+    for (size_t row = 0 ; row < row_size ; ++row) {
+        (*this)[row].ScaleInto(v[row], c);
+    }
+}
+
+// AddScaled(v, c) : this <- this + v * c
+template<class ValueType>
+void DenseMatrixImpl<ValueType>::AddScaled(const DenseMatrixImpl<ValueType>& v,
+                                           const ValueType& c) {
+    size_t row_size = this->RowSize();
+    for (size_t row = 0 ; row < row_size ; ++row) {
+        (*this)[row].AddScaled(v[row], c);
+    }
+}
+
+// AddScaledInto(u,v,c) : this <- u + v * c
+template<class ValueType>
+void DenseMatrixImpl<ValueType>::AddScaledInto(const DenseMatrixImpl<ValueType>& u,
+                                               const DenseMatrixImpl<ValueType>& v,
+                                               const ValueType& c)
+{
+    size_t row_size = this->RowSize();
+    for (size_t row = 0 ; row < row_size ; ++row) {
+        (*this)[row].AddScaled(u[row], v[row], c);
+    }
+}
 
 } // namespace brook
 
