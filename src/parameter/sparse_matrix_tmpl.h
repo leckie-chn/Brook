ec62b29fc54479176ae9e7e7f4248f5405a58469
dodolong <mctt90@gmail.com>
2015-03-22 12:58:02 +0800

    add sparse_matrix_tmpl.h

---
+++ src/parameter/sparse_matrix_tmpl.h
@@ -0,0 +1,25 @@
+// Copyright 2015 PKU-Cloud.
+// Author : Chao Ma (mctt90@gmail.com)
+//
+// Define the class template SparseMatrixImpl and operations required by
+// users.
+//
+#ifndef PARAMETER_SPARSE_MATRIX_TMPL_H_
+#define PARAMETER_SPARSE_MATRIX_TMPL_H_
+
+#include "src/parameter/sparse_vector_tmpl.h"
+
+#include <vector>
+
+namespace brook {
+
+using std::vector;
+
+template<class KeyType, class ValueType>
+class SparseMatrixImpl {
+
+};
+
+} // brook
+
+#endif // PARAMETER_SPARSE_MATRIX_TMPL_H_
