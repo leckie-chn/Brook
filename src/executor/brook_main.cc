3c6eeae7790d6899505f517dc9766368ae2abcbd
dodolong <mctt90@gmail.com>
2015-03-26 13:56:22 +0800

    update

---
+++ src/executor/brook_main.cc
@@ -0,0 +1,51 @@
+// Copyright 2015 PKU-Cloud.
+// Author : Chao Ma (mctt90@gmail.com)
+//
+#include <string>
+
+#include "src/util/common.h"
+#include "gflags/gflags.h"
+#include "src/executor/flags.h"
+#include "src/executor/brook.h"
+#include "src/executor/server.h"
+#include "src/executor/agent.h"
+#include "src/executor/master.h"
+
+namespace brook {
+
+bool Initialize();
+std::string WorkerType();
+void MasterWork();
+void ServerWork();
+void AgentWork();
+void Finalize();
+
+} // namespace brook
+
+//----------------------------------------------------------------------
+// The pre-defined main function
+//----------------------------------------------------------------------
+
+int main(int argc, char **argv) {
+    // Parse command line flags, leaving argc unchanged, but rearrange
+    // the arguments in argv so that the flags are all at the beginning.
+    google::ParseCommandLineFlags(&argc, &argv, false);
+
+    if (!brook::Initialize()) {
+        LOG(ERROR) << "Initialization of Brook failed.";
+        return -1;
+    }
+
+    LOG(INFO) << "I am a " << brook::WorkerType();
+
+    if (brook::WorkerType() == "Master") {
+        brook::MasterWork();
+    } else if (brook::WorkerType() == "Server") {
+        brook::ServerWork();
+    } else {
+        brook::AgentWork();
+    }
+
+    brook::Finalize();
+    return 0;
+}
