16eb0897c35ee84aa7ab4d0b0edd4d6d33fbd9ed
dodolong <mctt90@gmail.com>
2015-03-26 13:21:52 +0800

    update

--- src/executor/flags.cc
+++ src/executor/flags.cc
@@ -21,6 +21,49 @@
 #include "src/util/stringprintf.h"
 #include "src/util/split_string.h"
 
-DEFINE_int32(num_agent_workers, 0, "The num of agent workers.");
-DEFINE_int32(num_server_workers, 0, "The num of server workers.");
+namespace brook {
+
+const int kDefaultAgentOutputSize = 64 * 1024 * 1024; // 64 MB
+
+} // namespace brook
+
+DEFINE_int32(num_agent_workers, 0, 
+             "The num of agent workers.");
+
+DEFINE_int32(num_server_workers, 0, 
+             "The num of server workers.");
+
+DEFINE_string(cache_file_update, "", 
+              "The file pattern of updating cache file.");
+
+DEFINE_string(cache_file_parameter, "", 
+              "The file pattern of parameter cache file.");
+
+DEFINE_string(signal_file_update, "",
+              "The file name of updating signal fifo file.");
+
+DEFINE_string(signal_file_parameter, "",
+              "The file name of parameter signal fifo file.");
+
+DEFINE_string(log_filebase, "",
+              "The real log filename is log_filebase appended by worker "
+              "type, worker id, date, time, process_id, log type and etc.");
+
+DEFINE_int32(max_agent_output_size,
+             brook::kDefaultAgentOutputSize,
+             "The max size of a agent output, in bytes.");
+
+namespace brook {
+
+typedef std::vector<std::string> StringVector;
+
+//----------------------------------------------------------------------------
+// Check the correctness of flags.
+//----------------------------------------------------------------------------
+
+bool ValidateCommandLineFlags() {
+
+}
+
+} // namespace brook;
 
