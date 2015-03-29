69f2946acf06131dbc90f38092c4d69672fdda8f
dodolong <mctt90@gmail.com>
2015-03-29 21:10:58 +0800

    udpate

---
+++ src/consistency/consistency.cc
@@ -0,0 +1,6 @@
+// Copyright 2015 PKU-Cloud
+// Author : Chao Ma (mctt90@gmail.com)
+//
+namespace brook {
+
+} // namespace brook
---
+++ src/consistency/consistency.h
@@ -0,0 +1,11 @@
+// Copyright 2015 PKU-Cloud.
+// Author : Chao Ma (mctt90@gmail.com)
+//
+#ifndef CONSISTENCY_CONSISTENCY_H_
+#define CONSISTENCY_CONSISTENCY_H_
+
+namespace brook {
+
+} // namespace brook
+
+#endif // CONSISTENCY_CONSISTENCY_H_
