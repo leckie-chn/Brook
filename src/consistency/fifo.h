6d84c685a2609f1c77d63669600311c73c7c6cf4
dodolong <mctt90@gmail.com>
2015-03-29 15:14:30 +0800

    update

--- src/consistency/fifo.h
+++ src/consistency/fifo.h
@@ -6,10 +6,14 @@
 #ifndef CONSISTENCY_FIFO_H_
 #define CONSISTENCY_FIFO_H_
 
+#include "src/util/common.h"
+#include "src/util/logging.h"
+
 #include <string>
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #define EVERYONE_CAN_READ_AND_WRITE 0666
 
@@ -18,14 +22,14 @@ namespace brook {
 class Fifo {
 public:
     Fifo(std::string filename) : file_name_(filename) {
-
+        unlink(file_name_.c_str()); // Ensure close the pre-created file.
         if(mkfifo(file_name_.c_str(), EVERYONE_CAN_READ_AND_WRITE) != 0) {
             LOG(FATAL) << "Create fifo file failed.";
         }
     }
 
     ~Fifo() {
-
+        unlink(file_name_.c_str());
     }
 
 private:
---
+++ src/consistency/fifo_test.cc
@@ -0,0 +1,17 @@
+// Copyright 2015 PKU-Cloud.
+// Auhtor : Chao Ma (mctt90@gmail.com)
+//
+#include "src/consistency/fifo.h"
+
+#include <string>
+
+#include "gtest/gtest.h"
+
+using namespace std;
+using brook::Fifo;
+
+const string filename = "/tmp/test_fifo";
+
+TEST(FifoTest, CreateAndDestroy) {
+    Fifo fifo(filename);
+}
