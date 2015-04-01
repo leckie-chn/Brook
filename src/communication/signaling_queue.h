44a70d4b76f8b45bcbe0fd437d94c03af2db0230
dodolong <mctt90@gmail.com>
2015-04-01 16:09:55 +0800

    update

---
+++ src/communication/signaling_queue.h
@@ -0,0 +1,7 @@
+// Copyright 2015 PKU-Cloud.
+// Author : Chao Ma (mctt90@gmail.com)
+//
+#ifndef COMMUNICATION_SIGNALING_QUEUE_H_
+#define COMMUNICATION_SIGNALING_QUEUE_H_
+
+#endif // COMMUNICATION_SIGNALING_QUEUE_H_
