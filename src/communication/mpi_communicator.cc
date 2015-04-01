f607fe08466c1636c07e81f710ea1380282f0df2
dodolong <mctt90@gmail.com>
2015-04-01 14:19:46 +0800

    update

--- src/communication/mpi_communicator.cc
+++
@@ -1,5 +0,0 @@
-// Copyright 2015 PKU-Cloud.
-// Author : Chao Ma (mctt90@gmail.com)
-
-#include "src/communication/mpi_communicator.h"
-
--- src/communication/mpi_communicator.h
+++ src/communication/mpi_communicator.h
@@ -6,7 +6,10 @@
 
 #include <mpi.h>
 
+#include <string>
+
 #include "src/communication/communicator.h"
+#include "src/message/partition.h"
 
 namespace brook {
 
@@ -16,16 +19,40 @@ namespace brook {
 template <typename Message>
 class MPICommunictor : public Communicator<Message> {
 public:
-    MPICommunictor() {}
+    MPICommunictor(int buffer_size) : buffer_size_(buffer_size) { 
+        kAgentOutputTag_ = 1;
+    }
+    
     virtual ~MPICommunictor() {} 
 
-    virtual bool Initialize() { return true; }
-    virtual bool Finalize() { return true; }
+    virtual bool Initialize();
+    virtual bool Finalize();
 
     virtual int Send(Message&);
-    virtual int Receive(Message&);
+    virtual int Recieve(Message&);
+
+private:
+    int kAgentOutputTag_;
+    Partition partition_;
+    char *recieve_buffer_;
+    int buffer_size_;
 
 };
+
+template <typename Message>
+    int MPICommunictor<Message>::Send(Message& msg) {
+    std::string bytes;
+    msg.SerializeToString(&bytes);
+
+    MPI_Send(const_cast<char*>(bytes.data()), bytes.size(), MPI_CHAR,
+             partition_.NaiveShard(msg.start_index()), kAgentOutputTag_,
+             MPI_COMM_WORLD);
+}
+
+template <typename Message>
+int MPICommunictor<Message>::Recieve(Message& msg) {
+    MPI_Recv()
+}
 
 } // namespace brook
 
