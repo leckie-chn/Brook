e58c58c7c0c56549068907a70b742821cb467543
dodolong <mctt90@gmail.com>
2015-04-02 15:27:28 +0800

    update

--- src/communication/signaling_queue.h
+++ src/communication/signaling_queue.h
@@ -32,12 +32,39 @@ namespace brook {
 template <typename Message>
 class SignalingQueue {
 public:
-    SignalingQueue(int queue_size /*len*/,
+    SignalingQueue(int queue_size /*in bytes*/,
                    int num_producer = 1);
 
     ~SignalingQueue();
 
-    void Add(const )
+    // return : bytes added to queue
+    // > 0 : size of message
+    // = 0 : not enough space for this message (when is_blocking = false)
+    // - 1 : error
+    int Add(const char *src, int size, bool is_blocking = true);
+    int Add(const std::string &src, bool is_blocking = true);
+
+    // Remove a message from the queue
+    // return: bytes removed from queue
+    // > 0 : size of message
+    // = 0 : queue of message
+    //       invoke NoMoreAdd() to check if all producer have finished
+    // - 1 : fail
+    int Remove(char *dest, int max_size, bool is_blocking = true);
+    int Remove(std::string *dest, bool is_blocking = true);
+
+    // Signal that producer producer_id will no longer produce anything.
+    // After all num_producer_ producers invoked Signal, a special message is
+    // then inserted into the queue, so that the consumer can be notified to stop
+    // working.
+    void Signal(int producer_id);
+
+    // Returns true if queue is empty and all num_producer producers have
+    // Signaled their finish.
+    bool EmptyAndNoMoreAdd() const;
+
+private:
+
 };
 
 
