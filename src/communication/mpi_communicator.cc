1ea53057ae6f9a3451b1655190560cd4ff428e8a
dodolong <mctt90@gmail.com>
2015-04-03 17:38:49 +0800

    update

---
+++ src/communication/mpi_communicator.cc
@@ -0,0 +1,7 @@
+// Copyright 2015 PKU-Cloud.
+// Author : Chao Ma (mctt90@gmail.com)
+//
+
+namespace brook {
+
+} // namespace brook
--- src/communication/mpi_communicator.h
+++ src/communication/mpi_communicator.h
@@ -4,4 +4,14 @@
 #ifndef COMMUNICATION_MPI_ASY_COMMUNICATOR_H_
 #define COMMUNICATION_MPI_ASY_COMMUNICATOR_H_
 
+#include "src/communication/communicator.h"
+#include "src/communication/mpi_sendrecv.h"
+#include "src/communication/signaling_queue.h"
+
+namespace brook {
+
+
+
+} // namespace brook
+
 #endif // COMMUNICATION_MPI_ASY_COMMUNICATOR_H_
