c4b1503810a564200a0bb6984e07bee76cd79975
dodolong <mctt90@gmail.com>
2015-04-03 17:53:55 +0800

    update

--- src/communication/mpi_communicator.h
+++ src/communication/mpi_communicator.h
@@ -7,10 +7,35 @@
 #include "src/communication/communicator.h"
 #include "src/communication/mpi_sendrecv.h"
 #include "src/communication/signaling_queue.h"
+#include "src/util/common.h"
 
 namespace brook {
 
+class MPICommunicator : public Communicator {
+public:
+    MPICommunicator() {}
+    virtual ~MPICommunicator() {}
 
+    virtual 
+
+private:
+    bool is_sender_;
+    uint32 agent_queue_size_;
+    uint32 server_queue_size_;
+    
+    scoped_ptr<MPISendRecv> mpi_sendrecv_;
+    scoped_ptr<SignalingQueue> receive_buffer_;
+    scoped_ptr<SignalingQueue> send_buffer_;
+
+    scoped_ptr<boost::thread> thread_send_;
+    scoped_ptr<boost::thread> thread_receive_;
+
+    static void SendLoop(SocketCommunicator *pcom);
+    static void ReceiveLoop(SocketCommunicator *pcom);
+
+    DISALLOW_COPY_AND_ASSIGN(SocketCommunicator);
+
+};
 
 } // namespace brook
 
