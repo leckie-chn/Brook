94f09c73c5baf969fba580d71eb6607e99eee41a
dodolong <mctt90@gmail.com>
2015-04-09 20:31:20 +0800

    update

---
+++ src/parameter/random_queue_tmpl.h
@@ -0,0 +1,19 @@
+// Copyright 2015 PKU-Cloud.
+// Author : Chao Ma (mctt90@gmail.com)
+//
+#ifndef PARAMETER_RANDOM_QUEUE_TMPL_H_
+#define PARAMETER_RANDOM_QUEUE_TMPL_H_
+
+namespace brook {
+
+class RandomQueue {
+
+public:   
+
+private:
+
+};
+
+} // namespace brook
+
+#endif // PARAMETER_RANDOM_QUEUE_TMPL_H_
---
+++ src/parameter/random_queue_tmpl_test.cc
@@ -0,0 +1,4 @@
+// Copyright 2015 PKU-Cloud.
+// Author : Chao Ma (mctt90@gmail.com)
+//
+#include "src/parameter/random_queue_tmpl.h"
