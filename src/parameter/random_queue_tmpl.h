63dca041559c838c659c48ac9a3b5a537b92fdad
dodolong <mctt90@gmail.com>
2015-04-10 13:06:49 +0800

    update

--- src/parameter/random_queue_tmpl.h
+++ src/parameter/random_queue_tmpl.h
@@ -4,14 +4,42 @@
 #ifndef PARAMETER_RANDOM_QUEUE_TMPL_H_
 #define PARAMETER_RANDOM_QUEUE_TMPL_H_
 
+#include "src/util/common.h"
+
+#include <vector>
+
 namespace brook {
 
+template <class ValueType>
 class RandomQueue {
-
 public:   
+    RandomQueue();
+    ~RandomQueue();
+
+    void Push(ValueType& value) {
+        queue_.push_back(value);
+    }
+
+    ValueType Pop() {
+        ValueType res = queue_[0];
+        queue_.erase(queue_.begin());
+        return res;
+    }
+
+    ValueType& Get(int index) {
+        CHECK_GE(index, 0);
+        CHECK_LT(index, queue_.size());
+        return queue_[index];
+    }
+
+    void Set(int index, ValueType& value) {
+        CHECK_GE(index, 0);
+        CHECK_LT(index, queue_.size());
+        queue_[index] = value;
+    }
 
 private:
-
+    std::vector<ValueType> queue_;
 };
 
 } // namespace brook
