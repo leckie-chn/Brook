ccd4ce13c9984bb5527143f68d1c6c4ecc8ba02f
dodolong <mctt90@gmail.com>
2015-04-10 15:53:37 +0800

    update

--- src/parameter/version_buffer_tmpl.h
+++ src/parameter/version_buffer_tmpl.h
@@ -4,7 +4,7 @@
 #ifndef PARAMETER_VERSION_BUFFER_TMPL_H_
 #define PARAMETER_VERSION_BUFFER_TMPL_H_
 
-#include "src/parameter/dense_matrix_tmpl.h"
+#include "src/parameter/random_queue_tmpl.h"
 #include "src/parameter/dense_vector_tmpl.h"
 #include "src/util/common.h"
 #include "src/util/scoped_ptr.h"
@@ -15,49 +15,42 @@
 
 namespace brook {
 
-
-
 //-----------------------------------------------------------------------
 // VersionBuffer is used to store the updates in different iteration.
 //-----------------------------------------------------------------------
 template <class ValueType>
 class VersionBuffer {
 
-typedef DenseMatrixTmpl<ValueType> DenseMatrixTmpl;
+typedef RandomQueueTmpl<ValueType> RandomQueue;    
 typedef DenseVectorTmpl<ValueType> DenseVector;
 typedef std::vector<Bitmap> BitmapList;
 typedef std::vector<uint32> IntList;
+typedef std::vector<RandomQueue> RandomQueueList;
 
 public:
 
-    VersionBuffer(int bounded, uint64 feature_num, int num_agent) {
-        CHECK_GT(num_agent, 0);
-        CHECK_GE(bounded, 0);
+    VersionBuffer(uint64 feature_num, int bit_size, int num_agent) {
+        CHECK_GT(bit_size, 0);
         CHECK_GT(feature_num, 0);
+        CHECK_GT(num_agent, 0);
         
-        bounded_staleness_ = bounded;
-        row_size_ = bounded_staleness_ + 1;
         feature_num_ = feature_num;
+        bit_size_ = bit_size;
+        finished_count_ = 0;
         num_agent_ = num_agent;
-        bit_size_ = num_agent_ + 1;
-        agent_timestap_.resize(num_agent_, 0);
-        oldest_pointer_ = 0;
-        oldest_iteration_ = 0;
     
-        buffer_.reset(new DenseMatrixTmpl(row_size_), feature_num_, 0);
+        buffer_.reset(new RandomQueueList(feature_num_));
         accessing_table_.reset(new BitmapList(feature_num_, Bitmap(bit_size_)));
         accessing_count_.reset(new IntList(feature_num_, 0));
+        oldest_update_.reset(new DenseVector(feature_num_, 0));
 
-        for (int i = 0 ; i < row_size_ ; i++) {
-            iter_to_row_[i] = i;
-        }
     }
     
     ~VersionBuffer() {}
 
-    void Set(int row, uint64 key, ValueType& value);
+    void Set(uint64 key, uint32 iter_num, ValueType& value);
 
-    const ValueType& Get(int row, uint64 key);
+    const ValueType& Get(uint64 key, uint32 iter_num);
 
     void InsertUpdate(int worker_id, uint64 key, ValueType& value);
 
@@ -65,33 +58,29 @@ public:
 
 private:
 
-    scoped_ptr<DenseMatrixTmpl> buffer_;        // the buffer to store the version update data.
+    scoped_ptr<RandomQueueList> buffer_;        // the buffer to store the version update data.
     scoped_ptr<BitmapList> accessing_table_;    // to record each parameter has been accessed by a 
                                                 // list (bitmap) of agent.
     scoped_ptr<IntList> accessing_count_;       // to record the number of each parameter been accessed.
-   
-    int oldest_pointer_;                        // oldest_pointer_ record which row store the oldest updates.
-    int oldest_iteration_;                      // the oldest number of iteration.
-    int bounded_staleness_;                     // bounded_staleness_ decide the row size of buffer. 
-    int row_size_;                              // row_size = bounded_staleness_ + 1.                              
-    int bit_size_;                              // the size of each bitmap.
-    uint64 feature_num_;                        // the size of parameter, this may be vary large.
 
-    IntList agent_timestap_;                     // record the current timestap (iteration) of each agent worker.
-                                                 // NOTE: the real index of worker is index + 1, beacause we remove
-                                                 // master node.
-    std::map<uint32, uint32> iter_to_row_;       // mapping number of iteration to number of row.
-    int num_agent_;
+    scoped_ptr<DenseVector> oldest_update_;     // to store the oldest update.
+    uint32 finished_count_;                     // record how many iterations has been finished.
+                             
+    uint32 bit_size_;                           // the size of each bitmap.
+    uint64 feature_num_;                        // the size of parameter, this may be vary large.
+    uint32 num_agent_;
+
+    std::map<uint32, uint32> agent_timestap_;   // record the current timestap (iteration) of each agent worker.
 };
 
 template <class ValueType>
-void VersionBuffer<ValueType>::Set(int row, uint64 key, ValueType& value) {
-    (*buffer_)[row][key] = value;
+void VersionBuffer<ValueType>::Set(uint64 key, uint32 index, ValueType& value) {
+    (*buffer_)[key].Set(index, value);
 }
 
 template <class ValueType>
-const ValueType& VersionBuffer<ValueType>::Get(int row, uint64 key) {
-    return (*buffer_)[row][key];
+const ValueType& VersionBuffer<ValueType>::Get(uint64 key, uint32 index) {
+    return (*buffer_)[key].Get(index);
 }
 
 template <class ValueType>
@@ -99,12 +88,11 @@ void VersionBuffer<ValueType>::InsertUpdate(int worker_id, uint64 key, ValueType
     CHECK_GT(worker_id, 1);
     CHECK_LE(worker_id, num_agent_);
 
-    int timestap = agent_timestap_[worker_id - 1]; // NOTE: real worker id is index + 1, 
-                                                   // beacause we remove master node here.
-    int row = iter_to_row_[timestap];
-    Set(row, key, value + Get(row));    // Add new update to the buffer
+    int timestap = agent_timestap_[worker_id]; 
+    int index = timestap - finished_count_;
+    Set(key, index, value + Get(index));    // Add new update to the buffer
 
-    if (timestap == 0) {                // At the first iteration, we need to make the record.
+    if (timestap == 0) {                    // At the first iteration, we need to make the record.
         (*accessing_table_)[key].SetElement(worker_id);
         (*accessing_count_)[key]++;
     }
@@ -112,7 +100,11 @@ void VersionBuffer<ValueType>::InsertUpdate(int worker_id, uint64 key, ValueType
 
 template <class ValueType>
 DenseVectorTmpl<ValueType>& VersionBuffer<ValueType>::GetOldestUpdates() {
-    return (*buffer_)[oldest_pointer_];
+    for (uint64 i = 0 ; i < feature_num_ ; i++) {
+        oldest_update_[i] = buffer_[i].Pop();
+    }
+    finished_count_++;
+    return oldest_update_;
 }
 
 } // namespace brook
