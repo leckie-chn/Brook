e8c571ab0c4276a0e5525bf66181c5c473aea256
dodolong <mctt90@gmail.com>
2015-04-12 14:46:32 +0800

    update

---
+++ src/communication/zmq_communicator.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: zmq_communicator.h
+	> Author: 
+	> Mail: 
+	> Created Time: 日  4/12 14:46:15 2015
+ ************************************************************************/
+
+#ifndef _ZMQ_COMMUNICATOR_H
+#define _ZMQ_COMMUNICATOR_H
+#endif
